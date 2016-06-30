9ef69c48ebf41bf269d3749eb1ca1f43a52d7b90
Rounak Salim <rounaksalim@gmail.com>
2016-06-29 22:37:55 -0500

    Finished main crawling part, minor additions left

--- Java_Sourcecode_downloader.py
+++ Java_Sourcecode_downloader.py
@@ -17,17 +17,51 @@ def firstStage(methodName):
 
 	# make the query using the method name 
 	url = url + methodName
-	r = requests.get(url)
-
+	firstRequest = requests.get(url)
 	# create BeautifulSoup object for the site with query results
-	soup = BeautifulSoup(r.content, "html.parser")
+	soup = BeautifulSoup(firstRequest.content, "html.parser")
 
 	# find all results of search for div tags with class "result-list"
 	results = soup.find_all("div", {"class":"result-list"})
 
 	# get all links 
 	soup = BeautifulSoup(str(results), "html.parser")
-	links = soup.find_all("a")
+	link = soup.find("a")
 
-	for link in links:
-		print(link.get("href"))
+	print link.get("href")
+
+	finalUrl = link.get("href")
+
+	# after getting the url for the method documentation replace the .. with
+	# the website url to get the correct url 
+	finalUrl = finalUrl.replace("..", website)
+
+	# use requests to get content for the new url 
+	secondRequest = requests.get(finalUrl)
+
+	soup = BeautifulSoup(secondRequest.content, "html.parser")
+
+	rawDownloadLink = soup.find("a", title = "Download file")
+
+	# separate "../" from the new url 
+	downloadLinkHolder = rawDownloadLink.split("../")
+
+	# last index in downloadLinkHolder is what we need
+	# add full website url to get the correct download link 
+	downloadLink = website + downloadLinkHolder[len(downloadLinkHolder) - 1]
+
+	# use requests to get the required file 
+	download = requests.get(downloadLink)
+
+	# raise error is request was unsuccessful 
+	download.raise_for_status()
+
+	# make file object to write content to file 
+	# open in write binary mode to maintain Unicode encoding of text
+	finalFile = open(methodName + ".java", "wb")
+
+	# write data in chunks of bytes (100000 in this case) to the file 
+	for chunk in download.iter_content(100000):
+		finalFile.write(chunk)
+
+	finalFile.close()
